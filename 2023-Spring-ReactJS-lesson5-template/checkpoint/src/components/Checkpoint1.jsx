ddbbba28a472fa2806c8308d7351dca386672c69
Samitha Ranasinghe <73048542+Ranasinghe843@users.noreply.github.com>
2023-04-14 21:16:24 -0400

    upto lesson5

---
+++ 2023-Spring-ReactJS-lesson5-template/checkpoint/src/components/Checkpoint1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+export default function Checkpoint1() {
+  const pokemon = ["bulbasaur", "charmander", "squirtle"]
+  /* For Checkpoint 1: Create a mapper function to map each pokemon to a card component passing in the pokemon as a prop to the card. */
+
+  // TODO: Create a mapper function to map each pokemon to a card component passing in the pokemon as a prop to the card. Set the cards equal to the mapper's output
+  const cards = pokemon.map((poke) => <Card pokemon={poke}/>)
+  
+  return (
+    <div >
+      { cards }
+    </div>
+  );
+}
